d0a7eec0b53eca85e8fd0fe0e04e840c8e05f633
Munwoojin <tlstn2997kr@naver.com>
2022-04-17 19:51:51 +0900

    before use

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,15 @@ import User from "../models/User";
 export const getJoin = (req, res) => { res.render("join", { pageTitle: "Join" }); }
 export const postJoin = async (req, res) => {
     const { name, email, username, password, location, } = req.body;
+    const usernameExisits = await User.exists({username});
+    const pageTitle = "Join";
+    if(usernameExisits) {
+        return  res.render("join", {pageTitle, errorMessage: "This Username already exists"})
+    }
+    const emailExists = await User.exists({email});
+    if(emailExists) {
+        return res.render("join", {pageTitle, errorMessage: "This Email address already used"})
+    }
     await User.create({
         name, email, username, password, location,
     })
