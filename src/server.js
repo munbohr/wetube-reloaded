db96b3bd325579bfad900a0a4d47596918dce81b
Munwoojin <tlstn2997kr@naver.com>
2022-04-18 19:11:21 +0900

    use Session Two

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.get("/add-one",(req, res, next) => {
     req.session.potato += 1;
-    return res.send(`${req.session.id}\n${req.session.potato}`);
+    return res.send(`${req.session.id} ${req.session.potato}`);
 })
 
 
