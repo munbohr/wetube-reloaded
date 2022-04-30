a0ec7b3c856fae1f55071f1bd8312813efa460ba
Munwoojin <tlstn2997kr@naver.com>
2022-04-30 20:57:38 +0900

    Bugfix that when we upload videos my password hashing automatically

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
-import Video from "../models/Video";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
@@ -40,7 +40,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -55,10 +55,9 @@ export const postLogin = async (req, res) => {
     });
   }
   req.session.loggedIn = true;
-  req.session.user = user ;
+  req.session.user = user;
   return res.redirect("/");
 };
-
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
@@ -106,100 +105,99 @@ export const finishGithubLogin = async (req, res) => {
     ).json();
     const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
-      );
+    );
     if (!emailObj) {
+      // set notification
       return res.redirect("/login");
     }
-    let user = await User.findOne({email: emailObj.email})
-    if(!user) {
-     user = await User.create({
-      avatarUrl:userData.avatar_url,
-      name: userData.name,
-      username: userData.login,
-      email: emailObj.email,
-      password: "",
-      socialOnly: true,
-      location: userData.location,
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.avatar_url,
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
       });
-    } 
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect("/");
-      //create an account 
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
 };
-
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
-
-export const getEdit = (req, res) =>{
-  return res.render("edit-profile", {pageTitle: "Edit Profile"})
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
-
-export const postEdit  = async (req,res)=> {
-  const { 
-    session: { 
-      user: {_id, avatarUrl},
-    }, 
-    body:{name, email, username, location},
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id, avatarUrl },
+    },
+    body: { name, email, username, location },
     file,
-  } = req;  
-  console.log(file);
-  const updatedUser = await User.findByIdAndUpdate(_id, {avatarUrl:file? file.path: avatarUrl , name, email, username, location}, {new: true});
-  const exists = await User.find({ $or: [{ username }, { email }] });
-  if(exists) {
-    return res.render("edit-profile", {errorMessage:"this is already exists"})
-    /*return res.status(400).render("join", {
-      pageTitle,
-      errorMessage: "This username/email is already taken.",
-    });*/
-  } 
+  } = req;
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      avatarUrl: file ? file.path : avatarUrl,
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
   req.session.user = updatedUser;
   return res.redirect("/users/edit");
 };
-
 export const getChangePassword = (req, res) => {
-  if(req.session.user.socialOnly === true) {
+  if (req.session.user.socialOnly === true) {
     return res.redirect("/");
   }
-  return res.render("users/change-password", {pageTitle:"Change Password"})
-}
+  return res.render("users/change-password", { pageTitle: "Change Password" });
+};
 export const postChangePassword = async (req, res) => {
-  //send notification 
-  const { 
-    session: { 
-      user: {_id },
-    }, 
-    body:{oldPassword, newPassword, newPasswordConfirmation},
-  } = req;  
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { oldPassword, newPassword, newPasswordConfirmation },
+  } = req;
   const user = await User.findById(_id);
-  const ok = await bcrypt.compare(oldPassword,user.password);
-  if(!ok){
+  const ok = await bcrypt.compare(oldPassword, user.password);
+  if (!ok) {
     return res.status(400).render("users/change-password", {
-      pageTitle: "Change Password", 
+      pageTitle: "Change Password",
       errorMessage: "The current password is incorrect",
     });
   }
-  if(newPassword !== newPasswordConfirmation) {
-    return res.status(400).render("users/change-password", {pageTitle:"Change Password", errorMessage: "The password does not match "});
+  if (newPassword !== newPasswordConfirmation) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "The password does not match the confirmation",
+    });
   }
   user.password = newPassword;
   await user.save();
-  return res.redirect("/users/logout"); 
-}
+  return res.redirect("/users/logout");
+};
 
 export const see = async (req, res) => {
-  const {id} = req.params
+  const { id } = req.params;
   const user = await User.findById(id).populate("videos");
-  console.log(user);
-  if(!user) {
-    return res.status(404).render("404", {pageTitle: "User not found."}); 
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  const videos = await Video.find({owner: user._id});
-  console.log(videos);
-  return res.render("users/profile", {pageTitle: user.name, user, videos,});
+  return res.render("users/profile", {
+    pageTitle: user.name,
+    user,
+  });
 };
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,19 +26,27 @@ export const watch = async (req, res) => {
 
 export const getEdit = async (req, res) => {
     const { id } = req.params;
+    const {user: {_id}} = req.session
     const video = await Video.findById(id);
     if (!video) {
         return res.render("404", { pageTitle: "Video not found." });
+    }
+    if(String(video.owner) !== String(_id)) {
+        return res.status(403).redirect("/");
     }
     return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const postEdit = async (req, res) => {
+    const {user: {_id}} = req.session
     const { id } = req.params;
     const { title, description, hashtags } = req.body;
     const video = await Video.exists({ _id: id });
     if (!video) {
         return res.render("404", { pageTitle: "Video not found." });
+    }
+    if(String(video.owner) !== String(_id)) {
+        return res.status(403).redirect("/");
     }
     await Video.findByIdAndUpdate(id, {
         title,
@@ -76,9 +84,23 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
     const { id } = req.params;
+    const {
+    user: { _id },
+    } = req.session;
+    const video = await Video.findById(id);
+    const user = await User.findById(_id);
+    if(!video){
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+    }
+    if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
+    }
+    
     await Video.findByIdAndDelete(id);
-    return res.redirect("/")
-};
+    user.videos.splice(user.videos.indexOf(id),1);
+    user.save();
+    return res.redirect("/");
+    }
 
 export const search = async (req, res) => {
     const { keyword } = req.query;
