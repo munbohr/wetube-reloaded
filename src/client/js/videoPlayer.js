9ee8b0e71cd2fbfc69be24741055234a12aaa37b
Munwoojin <tlstn2997kr@naver.com>
2022-05-07 11:03:08 +0900

    design video-player & some ap Router

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,11 +1,14 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const playMuteIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
 const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
 
@@ -26,7 +29,7 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 // if user click the muteBtn
@@ -36,7 +39,9 @@ const handleMuteClick = (e) => {
   } else {
     video.muted = true;
   }
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  playMuteIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -78,10 +83,10 @@ const handleFullscreen = () => {
   const fullscreen = document.fullscreenElement;
   if (fullscreen) {
     document.exitFullscreen();
-    fullScreenBtn.innerText = `Enter Full Screen`;
+    fullScreenIcon.classList = `fas fa-expend`;
   } else {
     videoContainer.requestFullscreen();
-    fullScreenBtn.innerText = `Exit Full Screen`;
+    fullScreenIcon.classList = `fas fa-compress`;
   }
 };
 
@@ -112,7 +117,7 @@ muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
 timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullscreen);
-video.addEventListener("mousemove", handleMouseMove);
-video.addEventListener("mouseleave", handleMouseLeave);
--- src/server.js
+++ src/server.js
@@ -1,37 +1,40 @@
 import express from "express";
 import morgan from "morgan";
-import session from "express-session"; 
+import session from "express-session";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import {localsMiddleware} from "./middlewares";
+import apiRouter from "./routers/apiRouter";
+import { localsMiddleware } from "./middlewares";
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views")
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 console.log(process.env.DB_URL);
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 20000,
-        //이러면 sessionID가 20초 후에 사라진다.
+      maxAge: 20000,
+      //이러면 sessionID가 20초 후에 사라진다.
     },
-    store: MongoStore.create({mongoUrl: process.env.DB_URL})
-}))
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
 
-
-app.use(localsMiddleware);  
+app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;
