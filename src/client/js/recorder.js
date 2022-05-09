272be721fedb802f086633a2c3747d0524e29fa4
Munwoojin <tlstn2997kr@naver.com>
2022-05-09 10:00:50 +0900

    downloading recorded video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,27 +2,31 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+
+const handleDownload = () => {};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
   startBtn.addEventListener("click", handleStart);
+  recorder.stop();
 };
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
-  recorder.ondataavailable = (e) => {
-    console.log("recordin done");
-    console.log(e);
-    console.log(e.data);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log(event.data);
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
   recorder.start();
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
 };
 
 const init = async () => {
